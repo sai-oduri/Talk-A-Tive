858a4fea8f8903fcee5e74b33e0d19534afc5c36
sai-oduri <saioduri95@gmail.com>
2024-10-05 13:22:12 +0530

    added functionality for login and signup pages

--- frontend/src/components/authentication/Login.jsx
+++ frontend/src/components/authentication/Login.jsx
@@ -1,14 +1,68 @@
 import { Button, FormControl, FormLabel, Input, InputGroup, InputRightElement, VStack } from '@chakra-ui/react'
 import React, { useState } from 'react'
+import { useToast } from '@chakra-ui/react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
 
     const [email, setEmail] = useState();
     const [password, setPassword] = useState();
     const [show, setShow] = useState(false);
+    const [loading, setLoading] = useState(false);
 
-    const submitHandler = () => {
+    const toast = useToast();
+    const history = useNavigate();
 
+    const submitHandler = async () => {
+        setLoading(true);
+        if (!email || !password) {
+            toast({
+                title: 'Please Fill all the Fields',
+                status: 'warning',
+                duration: 5000,
+                isClosable: true,
+                position: "bottom",
+            });
+            setLoading(false);
+            return;
+        }
+
+        try {
+            const config = {
+                headers: {
+                    "Content-type": "application/json",
+                },
+            };
+
+            const { data } = await axios.post("/api/user/login", {
+                email,
+                password,
+            }, config);
+
+            toast({
+                title: 'Login Successful',
+                status: 'success',
+                duration: 5000,
+                isClosable: true,
+                position: "bottom",
+            });
+
+            localStorage.setItem("userInfo", JSON.stringify(data));
+            setLoading(false);
+            history('/chats')
+
+        } catch (error) {
+            toast({
+                title: 'Error Occured!',
+                description: error.respose.data.message,
+                status: 'warning',
+                duration: 5000,
+                isClosable: true,
+                position: "bottom",
+            });
+            setLoading(false);
+        }
     }
 
 
@@ -17,13 +71,13 @@ const Login = () => {
 
             <FormControl id="email" isRequired>
                 <FormLabel>Email</FormLabel>
-                <Input placeholder='Enter Your Email' onChange={(e) => setEmail(e.target.value)}></Input>
+                <Input value={email} placeholder='Enter Your Email' onChange={(e) => setEmail(e.target.value)}></Input>
             </FormControl>
 
             <FormControl id="password" isRequired>
                 <FormLabel>Password</FormLabel>
                 <InputGroup>
-                    <Input type={show ? "text" : "password"} placeholder='Password' onChange={(e) => setPassword(e.target.value)}></Input>
+                    <Input value={password} type={show ? "text" : "password"} placeholder='Password' onChange={(e) => setPassword(e.target.value)}></Input>
                     <InputRightElement w="4.5rem">
                         <Button h="1.75rem" size="sm" onClick={() => setShow(!show)}>
                             {show ? "Hide" : "Show"}
@@ -33,7 +87,7 @@ const Login = () => {
             </FormControl>
 
 
-            <Button colorScheme='blue' width="100%" style={{ marginTop: 15 }} onClick={submitHandler}  >
+            <Button colorScheme='blue' width="100%" style={{ marginTop: 15 }} onClick={submitHandler} isLoading={loading} >
                 Login
             </Button>
 
--- frontend/src/components/authentication/Signup.jsx
+++ frontend/src/components/authentication/Signup.jsx
@@ -1,5 +1,8 @@
-import { Button, FormControl, FormLabel, Input, InputGroup, InputRightElement, VStack } from '@chakra-ui/react'
-import React, { useState } from 'react'
+import { Button, FormControl, FormLabel, Input, InputGroup, InputRightElement, VStack } from '@chakra-ui/react';
+import { useToast } from '@chakra-ui/react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import React, { useState } from 'react';
 
 const Signup = () => {
 
@@ -9,13 +12,120 @@ const Signup = () => {
     const [confirmpassword, setConfirmpassword] = useState();
     const [pic, setPic] = useState();
     const [show, setShow] = useState(false);
+    const [loading, setLoading] = useState(false);
+
+    const history = useNavigate();
+
+    const toast = useToast();
 
     const postDetails = (pics) => {
+        setLoading(true);
+        if (pics === undefined) {
+            toast({
+                title: 'Please Select an Image!',
+                status: 'warning',
+                duration: 5000,
+                isClosable: true,
+                position: "bottom",
+            });
+            setLoading(false);
+            return;
+        }
+
+        if (pics.type === "image/jpeg" || pics.type === "image/png") {
+            const data = new FormData();
+            data.append("file", pics);
+            data.append("upload_preset", "chat-app");
+            data.append("cloud_name", "dqkbtpgum");
+
+            fetch("https://api.cloudinary.com/v1_1/dqkbtpgum/image/upload", {
+                method: 'post',
+                body: data,
+            })
+                .then((res) => res.json())
+                .then((data) => {
+                    setPic(data.url.toString());
+                    setLoading(false);
+                })
+                .catch((err) => {
+                    console.log(err);
+                    setLoading(false);
+                });
+        } else {
+            toast({
+                title: 'Please Select a valid Image!',
+                status: 'warning',
+                duration: 5000,
+                isClosable: true,
+                position: "bottom",
+            });
+            setLoading(false);
+            return;
+        }
 
     }
 
-    const submitHandler = () => {
+    const submitHandler = async () => {
+        setLoading(true);
+        if (!name || !email || !password || !confirmpassword) {
+            toast({
+                title: 'Please Fill all the Fields',
+                status: 'warning',
+                duration: 5000,
+                isClosable: true,
+                position: "bottom",
+            });
+            setLoading(false);
+            return;
+        }
 
+        if (password !== confirmpassword) {
+            toast({
+                title: 'Passwords Do Not Match',
+                status: 'warning',
+                duration: 5000,
+                isClosable: true,
+                position: "bottom",
+            });
+            return;
+        }
+
+        try {
+            const config = {
+                headers: {
+                    "Content-type": "application/json",
+                },
+            };
+
+            const { data } = await axios.post("/api/user", {
+                name,
+                email,
+                password,
+                pic,
+            }, config);
+
+            toast({
+                title: 'Registration Successful',
+                status: 'success',
+                duration: 5000,
+                isClosable: true,
+                position: "bottom",
+            });
+
+            localStorage.setItem("userInfo", JSON.stringify(data));
+            setLoading(false);
+            history('/chats')
+        } catch (error) {
+            toast({
+                title: 'Error Occured!',
+                description: error.respose.data.message,
+                status: 'warning',
+                duration: 5000,
+                isClosable: true,
+                position: "bottom",
+            });
+            setLoading(false);
+        }
     }
 
 
@@ -58,11 +168,11 @@ const Signup = () => {
 
             <FormControl id="pic">
                 <FormLabel>Upload your Picture</FormLabel>
-                <Input type="file" p={1} accept="image/*" onChange={postDetails} />
+                <Input type="file" p={1} accept="image/" onChange={(e) => postDetails(e.target.files[0])} />
             </FormControl>
 
-            <Button colorScheme='blue' width="100%" style={{ marginTop: 15 }} onClick={submitHandler}  >
-                SIgn Up
+            <Button colorScheme='blue' width="100%" style={{ marginTop: 15 }} onClick={submitHandler} isLoading={loading}  >
+                Sign Up
             </Button>
 
         </VStack >
